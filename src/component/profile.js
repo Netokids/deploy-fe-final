0e366ffae35d88c3f78e312284f98cd671cf5b10
Dion Novalino <dionovalino@gmail.com>
2023-01-15 15:44:51 +0700

    Deploy Final Task

--- src/component/profile.js
+++ src/component/profile.js
@@ -223,27 +223,45 @@ const Profile = () => {
                                 <div className="p-2" style={{
                                     width: '40%',
                                     margin: '20px'
-                                }}>
-                                    <img src={item?.image} alt='' style={{
-                                        width: '280px',
-                                        height: '345px',
-                                        marginTop: '70px'
-                                    }}></img>
-                                    <Form.Control
-                                        type="file"
-                                        id="image"
-                                        className="form-input input-image"
-                                        name="image"
-                                        onChange={handleSubmitImage.mutate}
-                                    />
-                                    <Button onClick={() => {
-                                        document.getElementById("image").click();
-                                    }} style={{
-                                        width: '100%',
-                                        marginTop: '20px',
-                                        backgroundColor: '#FF6185',
-                                        marginTop: '-70px',
-                                    }}>Change Photo Profile</Button>
+                                }}>{item?.image === ""
+                                    ?
+                                    <>
+                                        <img src={Image} alt='' style={{
+                                            width: '280px',
+                                            height: '345px',
+                                            marginTop: '70px'
+                                        }}>
+
+                                        </img>
+                                    </>
+                                    :
+                                    <>
+                                        <img src={item?.image} alt='' style={{
+                                            width: '280px',
+                                            height: '345px',
+                                            marginTop: '70px'
+                                        }}>
+
+                                        </img>
+                                        <Form.Control
+                                            type="file"
+                                            id="image"
+                                            className="form-input input-image"
+                                            name="image"
+                                            onChange={handleSubmitImage.mutate}
+                                        />
+                                        <Button onClick={() => {
+                                            document.getElementById("image").click();
+                                        }} style={{
+                                            width: '100%',
+                                            marginTop: '20px',
+                                            backgroundColor: '#FF6185',
+                                            marginTop: '-70px',
+                                        }}>Change Photo Profile</Button>
+                                    </>
+                                    }
+
+
                                 </div>
                             </div>
                         </>
